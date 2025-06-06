3cb395a36909d003973f541db40c5370052edc92
Navaneeth Prabha <76899241+Navaneethp007@users.noreply.github.com>
2025-06-07 00:04:57 +0530

    record logic

--- App.js
+++ App.js
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
-export default function App() {
+const App = () => { 
+  const [rec, setRec] = useState(null);
+  const [isrec, setIsRec] = useState(false);
+
+  const start = async () => {
+    try {
+      const { status } = await Audio.requestPermissionsAsync();
+      if (status !== 'granted') {
+        alert('Permission to access microphone was denied');
+        return;
+      }
+      Audio.setAudioModeAsync({
+        allowsRecordingIOS: true,
+        playsInSilentModeIOS: true,
+      });
+      const { recording } = await Audio.Recording.createAsync(
+        Audio.RecordingOptionsPresets.HIGH_QUALITY
+      );
+      setRec(recording);
+      setIsRec(true);
+    } catch (error) {
+      alert('Error starting recording:');
+    }
+  };
+
+  const stop = async () => {
+    try {
+      await rec.stopAndUnloadAsync();
+      setRec(null);
+      setIsRec(false);
+    } catch (error) {
+      alert('Error stopping recording:');
+    }
+      
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Audio Recording App</Text>
+      <Button
+        title={isrec ? 'Stop Recording' : 'Start Recording'}
+        onPress={isrec ? stop : start}
+      />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default App;
